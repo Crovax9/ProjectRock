28db06378108a08f8e95c2c2efece90dcb90da67
user150 <crovax9@gmail.com>
2018-03-13 22:42:30 +0900

    BounceZone Scripts Add
    BounceZone Scripts Add,

---
+++ ProjectRock/Assets/Scripts/BounceZone.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceZone : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.back * 20000.0f);
+    }
+}
