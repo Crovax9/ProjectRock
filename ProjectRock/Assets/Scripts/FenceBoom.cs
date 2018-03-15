e7a7e75bda9b85e07a9b80a2fdfb60e8abc250c7
user150 <crovax9@gmail.com>
2018-03-15 18:11:30 +0900

    MeshSplit Scripts Add
    ExplosionEffectFunction Add

---
+++ ProjectRock/Assets/Scripts/FenceBoom.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FenceBoom : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.name == "Sphere")
+        {
+            this.GetComponent<BoxCollider>().enabled = false;
+            this.transform.GetComponentInChildren<MeshSplit>().enabled = true;
+        }
+    }
+}
