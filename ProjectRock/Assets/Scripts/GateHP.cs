77e131cd897fe297384c16bd6bc036a7618e447f
user150 <crovax9@gmail.com>
2018-03-22 19:51:32 +0900

    Goal Scripts Add
    Gola Scripts Add

---
+++ ProjectRock/Assets/Scripts/GateHP.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateHP : MonoBehaviour
+{
+    static float hp = 100;
+    private void OnTriggerEnter(Collider other)
+    {
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        hp -= rb.velocity.sqrMagnitude * 0.1f;
+        Debug.Log(hp);
+        if (hp > 0)
+        {
+            other.transform.position = new Vector3(2052, 512, 135);
+        }
+        else
+        {
+            this.GetComponent<MeshSplit>().enabled = true;
+            this.GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+}
